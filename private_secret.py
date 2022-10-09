eb620a55539e92ae99fcd22666458c40616cf7e5
w0_0jnn <godqhr1901@naver.com>
2022-10-10 04:29:43 +0900

    Lms 비밀글 읽기_초판

---
+++ private_secret.py
@@ -0,0 +1,103 @@
+import requests
+from bs4 import BeautifulSoup
+
+main_url = "https://lms.ksa.hs.kr"
+login_url = main_url + "/Source/Include/login_ok.php"
+docu_url_prefix = "https://lms.ksa.hs.kr/nboard.php?db=vod&mode=view&idx="
+doce_url_suffix = "&page=1&ss=on&sc=&sn=&db=vod&scBCate=133"
+
+session = requests.session()
+
+login_info = {
+    'user_id' : '22-096',
+    'user_pwd' : 'test'
+}
+
+res = session.post(login_url, data = login_info)
+res.raise_for_status()
+
+def binary_search(find_name, find_date):
+    start = 0
+    end = 103094
+
+    while start <= end:
+        mid = (start + end) // 2
+        output = search(str(mid))
+        if not output:
+            start -= 1
+            continue
+        print(str(mid)) # Test용
+        if find_name in output[0] and find_date in output[1]:
+            return mid
+        elif find_date == output[1]:
+            first_mid = mid
+            while find_date == output[1]:
+                mid += 1
+                temp = search(str(mid))
+                print(str(mid)) # Test용
+                if temp:
+                    output = temp
+                    print(output[0], find_name, output[0] == find_name)
+                    if find_name == output[0] and find_date == output[1]:
+                        return mid
+            mid = first_mid
+            output = search(str(mid))
+            while find_date == output[1]:
+                mid -= 1
+                temp = search(str(mid))
+                print(str(mid)) # Test용
+                if temp:
+                    output = temp
+                    print(output[0], find_name, output[0] == find_name)
+                    if find_name == output[0] and find_date == output[1]:
+                        return mid
+            return None
+        elif output[1] < find_date:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return None
+        
+def search(id):
+    res = session.get(make_link(id))
+    res.raise_for_status()
+
+    soup = BeautifulSoup(res.text, "html.parser")
+    name_date = soup.find_all(attrs={'class':'blue01'})
+    
+    if not name_date:
+        return None
+    
+    name = get_name(name_date)
+    date = get_date(name_date)
+    
+    return name, date
+
+def get_date(date):
+    date = date[0].get_text()
+    for i in range(len(date)):
+        if date[i] == "록" and date[i+1] == "일":
+            date = date[i+4:i+8] + date[i+9:i+11] + date[i+12:i+14]
+            return date
+    return None
+
+def get_name(name):
+    name = name[0].get_text()
+    for i in range(len(name)):
+        if name[i] == "이" and name[i+1] == "름":
+            name = name[i+4:i+7]
+            return name
+    return None
+
+def make_link(id):
+    return docu_url_prefix + str(id) + doce_url_suffix
+
+find_name = input("찾을 게시글의 작성자를 입력하세요: ")
+find_date = input("찾을 게시글의 날짜를 입력하세요(예: 20200101): ")
+
+output = binary_search(find_name, find_date)
+
+if output:
+    print(make_link(output))
+else:
+    print("찾는 게시글이 없습니다.")
